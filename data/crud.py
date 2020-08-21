0a48b11a50c5e6b474e7cafd5f032cc9f56f5bf2
Alex Pishcheiko <oleksandra.pishcheiko@distilled.ie>
2020-08-21 14:24:35 +0100

    endpoint to update project name

--- data/crud.py
+++ data/crud.py
@@ -783,3 +783,12 @@ class Update:
         session_commit()
 
         return test_history.id
+    
+    @staticmethod
+    def update_project_name(id, name):
+        project = db.session.query(models.Project).get(id)
+        project.name = name
+
+        session_commit()
+
+        return project.name
